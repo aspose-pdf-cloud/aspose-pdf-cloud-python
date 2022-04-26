7f12adbbe6369c813a4e881d9a4feb6f45fffbd8
Oleg Subachev <Oleg.Subachev@aspose.com>
2022-04-26 03:33:28 -0500

    update to 22.4

--- asposepdfcloud/configuration.py
+++ asposepdfcloud/configuration.py
@@ -199,5 +199,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 3.0\n"\
-               "SDK Package Version: 22.3.0".\
+               "SDK Package Version: 22.4.0".\
                format(env=sys.platform, pyversion=sys.version)
