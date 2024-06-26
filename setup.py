07470aa81196ba793958e9440b304f06f333e2e3
Kirill Novinskiy <kirill.novinskiy@aspose.com>
2024-06-26 06:59:24 -0500

    update to 24.6

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "asposepdfcloud"
-VERSION = "24.5.0"
+VERSION = "24.6.0"
 # To install the library, run the following
 #
 # python setup.py install
