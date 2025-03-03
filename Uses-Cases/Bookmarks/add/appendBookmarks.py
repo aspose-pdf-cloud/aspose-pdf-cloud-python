7e9f1fa6c32c6c11ba488c506e07b5eca431264f
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-03-03 10:57:20 +0200

    Update appendBookmarks.py

--- Uses-Cases/Bookmarks/add/appendBookmarks.py
+++ Uses-Cases/Bookmarks/add/appendBookmarks.py
@@ -2,7 +2,7 @@ import shutil
 import json
 import logging
 from pathlib import Path
-from asposepdfcloud import ApiClient, PdfApi, Link, Color, Bookmark
+from asposepdfcloud import ApiClient, PdfApi, Color, Bookmark
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
@@ -13,7 +13,7 @@ class Config:
     LOCAL_FOLDER = Path(r"C:\Samples")
     PDF_DOCUMENT_NAME = "sample.pdf"
     LOCAL_RESULT_DOCUMENT_NAME = "output_sample.pdf"
-    NEW_BOOKMARK_TITLE = "• Productivity improvement"
+    NEW_BOOKMARK_TITLE = "• Increased performance.." #"• Productivity improvement"
     PARENT_BOOKMARK_FOR_APPEND = ""                 #The parent bookmark path. Specify an empty string when adding a bookmark to the root.
     NEW_BOOKMARK_PAGE_NUMBER = 3
     BOOKMARK_PAGE_POSITION_X = 89
@@ -38,65 +38,52 @@ class PdfBookmarks:
         except (FileNotFoundError, json.JSONDecodeError, ValueError) as e:
             logging.error(f"init_api(): Failed to load credentials: {e}")
 
-    def _ensure_api_initialized(self):
-        """Check if the API is initialized before making API calls."""
-        if not self.pdf_api:
-            logging.error("ensure_api_initialized(): PDF API is not initialized. Operation aborted.")
-            return False
-        return True
-
     def upload_document(self):
         """Upload a PDF document to the Aspose Cloud server."""
-        if not self._ensure_api_initialized():
-            return
-
-        file_path = Config.LOCAL_FOLDER / Config.PDF_DOCUMENT_NAME
-        try:
-            self.pdf_api.upload_file(Config.PDF_DOCUMENT_NAME, str(file_path))
-            logging.info(f"upload_document(): File {Config.PDF_DOCUMENT_NAME} uploaded successfully.")
-        except Exception as e:
-            logging.error(f"upload_document(): Failed to upload file: {e}")
+        if self.pdf_api:
+            file_path = Config.LOCAL_FOLDER / Config.PDF_DOCUMENT_NAME
+            try:
+                self.pdf_api.upload_file(Config.PDF_DOCUMENT_NAME, str(file_path))
+                logging.info(f"upload_document(): File {Config.PDF_DOCUMENT_NAME} uploaded successfully.")
+            except Exception as e:
+                logging.error(f"upload_document(): Failed to upload file: {e}")
 
     def download_result(self):
         """Download the processed PDF document from the Aspose Cloud server."""
-        if not self._ensure_api_initialized():
-            return
-
-        try:
-            temp_file = self.pdf_api.download_file(Config.PDF_DOCUMENT_NAME)
-            local_path = Config.LOCAL_FOLDER / Config.LOCAL_RESULT_DOCUMENT_NAME
-            shutil.move(temp_file, str(local_path))
-            logging.info(f"download_result(): File successfully downloaded: {local_path}")
-        except Exception as e:
-            logging.error(f"download_result(): Failed to download file: {e}")
+        if self.pdf_api:
+            try:
+                temp_file = self.pdf_api.download_file(Config.PDF_DOCUMENT_NAME)
+                local_path = Config.LOCAL_FOLDER / Config.LOCAL_RESULT_DOCUMENT_NAME
+                shutil.move(temp_file, str(local_path))
+                logging.info(f"download_result(): File successfully downloaded: {local_path}")
+            except Exception as e:
+                logging.error(f"download_result(): Failed to download file: {e}")
 
     def append_bookmark_link(self):
         """Append a new bookmark link to a specific page in the PDF document."""
-        if not self._ensure_api_initialized():
-            return
-
-        newBookmark = Bookmark(
-            title = Config.NEW_BOOKMARK_TITLE,
-            italic = True,
-            bold = True,
-            color = Color(a=255,r=0,g=255,b=0),
-            level = 1,
-            page_display_left = Config.BOOKMARK_PAGE_POSITION_X,
-            page_display_top = Config.BOOKMARK_PAGE_POSITION_Y,
-            page_display_zoom = 2,
-            page_number = Config.NEW_BOOKMARK_PAGE_NUMBER
-        )
-
-        try:
-            response = self.pdf_api.post_bookmark(
-                Config.PDF_DOCUMENT_NAME, Config.PARENT_BOOKMARK_FOR_APPEND, [newBookmark]
+        if self.pdf_api:
+            newBookmark = Bookmark(
+                title = Config.NEW_BOOKMARK_TITLE,
+                italic = True,
+                bold = True,
+                color = Color(a=255,r=0,g=255,b=0),
+                level = 1,
+                page_display_left = Config.BOOKMARK_PAGE_POSITION_X,
+                page_display_top = Config.BOOKMARK_PAGE_POSITION_Y,
+                page_display_zoom = 2,
+                page_number = Config.NEW_BOOKMARK_PAGE_NUMBER
             )
-            if response.code == 200:
-                logging.info(f"append_bookmark_link(): Bookmark '{response.bookmarks.list[0].action}'->'{Config.NEW_BOOKMARK_TITLE}' added to page #{Config.NEW_BOOKMARK_PAGE_NUMBER}.")
-            else:
-                logging.error(f"append_bookmark_link(): Failed to add bookmark '{Config.NEW_BOOKMARK_TITLE}' to the page #{Config.NEW_BOOKMARK_PAGE_NUMBER}. Response code: {response.code}")
-        except Exception as e:
-            logging.error(f"append_bookmark_link(): Error while adding bookmark: {e}")
+
+            try:
+                response = self.pdf_api.post_bookmark(
+                    Config.PDF_DOCUMENT_NAME, Config.PARENT_BOOKMARK_FOR_APPEND, [newBookmark]
+                )
+                if response.code == 200:
+                    logging.info(f"append_bookmark_link(): Bookmark '{response.bookmarks.list[0].action}'->'{Config.NEW_BOOKMARK_TITLE}' added to page #{Config.NEW_BOOKMARK_PAGE_NUMBER}.")
+                else:
+                    logging.error(f"append_bookmark_link(): Failed to add bookmark '{Config.NEW_BOOKMARK_TITLE}' to the page #{Config.NEW_BOOKMARK_PAGE_NUMBER}. Response code: {response.code}")
+            except Exception as e:
+                logging.error(f"append_bookmark_link(): Error while adding bookmark: {e}")
 
 
 if __name__ == "__main__":
