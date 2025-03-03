6e6eee354e7702b36cae38048827827882b84ea0
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-03-03 10:59:14 +0200

    Update removeBookmark.py

--- Uses-Cases/Bookmarks/remove/removeBookmark.py
+++ Uses-Cases/Bookmarks/remove/removeBookmark.py
@@ -36,48 +36,35 @@ class PdfBookmarks:
         except (FileNotFoundError, json.JSONDecodeError, ValueError) as e:
             logging.error(f"Failed to load credentials: {e}")
 
-    def _ensure_api_initialized(self):
-        """Check if the API is initialized before making API calls."""
-        if not self.pdf_api:
-            logging.error("PDF API is not initialized. Operation aborted.")
-            return False
-        return True
-
     def upload_document(self):
         """Upload a PDF document to the Aspose Cloud server."""
-        if not self._ensure_api_initialized():
-            return
-
-        file_path = Config.LOCAL_FOLDER / Config.PDF_DOCUMENT_NAME
-        try:
-            self.pdf_api.upload_file(Config.PDF_DOCUMENT_NAME, str(file_path))
-            logging.info(f"File {Config.PDF_DOCUMENT_NAME} uploaded successfully.")
-        except Exception as e:
-            logging.error(f"Failed to upload file: {e}")
+        if self.pdf_api:
+            file_path = Config.LOCAL_FOLDER / Config.PDF_DOCUMENT_NAME
+            try:
+                self.pdf_api.upload_file(Config.PDF_DOCUMENT_NAME, str(file_path))
+                logging.info(f"File {Config.PDF_DOCUMENT_NAME} uploaded successfully.")
+            except Exception as e:
+                logging.error(f"Failed to upload file: {e}")
 
     def download_result(self):
         """Download the processed PDF document from the Aspose Cloud server."""
-        if not self._ensure_api_initialized():
-            return
-
-        try:
-            temp_file = self.pdf_api.download_file(Config.PDF_DOCUMENT_NAME)
-            local_path = Config.LOCAL_FOLDER / Config.LOCAL_RESULT_DOCUMENT_NAME
-            shutil.move(temp_file, str(local_path))
-            logging.info(f"download_result(): File successfully downloaded: {local_path}")
-        except Exception as e:
-            logging.error(f"download_result(): Failed to download file: {e}")
+        if self.pdf_api:
+            try:
+                temp_file = self.pdf_api.download_file(Config.PDF_DOCUMENT_NAME)
+                local_path = Config.LOCAL_FOLDER / Config.LOCAL_RESULT_DOCUMENT_NAME
+                shutil.move(temp_file, str(local_path))
+                logging.info(f"download_result(): File successfully downloaded: {local_path}")
+            except Exception as e:
+                logging.error(f"download_result(): Failed to download file: {e}")
 
     def remove_bookmark_by_path(self):
-        if not self.pdf_api:
-            return
+        if self.pdf_api:
+            response: AsposeResponse = self.pdf_api.delete_bookmark(Config.PDF_DOCUMENT_NAME, Config.BOOKMARK_PATH)
 
-        response: AsposeResponse = self.pdf_api.delete_bookmark(Config.PDF_DOCUMENT_NAME, Config.BOOKMARK_PATH)
-
-        if response.code == 200:
-            logging.info(f"Bookmark with path: '{Config.BOOKMARK_PATH}' has been removed.")
-        else:
-            logging.erro(f"Failed to remove bookmark with path: '{Config.LINK_FIND_ID}'.")
+            if response.code == 200:
+                logging.info(f"Bookmark with path: '{Config.BOOKMARK_PATH}' has been removed.")
+            else:
+                logging.erro(f"Failed to remove bookmark with path: '{Config.LINK_FIND_ID}")
 
 if __name__ == "__main__":
     pdf_bookmarks = PdfBookmarks()
