894401a0d4983409c40407c4656d27c6ddaa69f7
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-29 23:00:17 +0300

    PDFCLOUD-4979: added snippets for Compare

---
+++ Uses-Cases/Compares/comapres_launch.py
@@ -0,0 +1,7 @@
+from compares_helper import CompareMain
+from comapre_pdf_documents import Config, ComparePdfDocuments
+
+if __name__ == "__main__":
+    comparator = CompareMain(Config.CREDENTIALS_FILE)
+    pdf_compares = ComparePdfDocuments(comparator.pdf_api, comparator)
+    pdf_compares.compareDocument()
