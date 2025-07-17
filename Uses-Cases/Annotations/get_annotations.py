f7c55683933b6c677aade0ca00e9b5bff7d0c658
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-17 22:44:29 +0300

    Update get_annotations.py

--- Uses-Cases/Annotations/get_annotations.py
+++ Uses-Cases/Annotations/get_annotations.py
@@ -7,7 +7,7 @@ class PdfGetAnnotations:
         self.pdfApi = pdf_api
         self.helper = helper
 
-    def get_annotations(self):
+    def request_annotations(self):
         """Get annotations from the page in the PDF document."""
         if self.pdfApi:
             self.helper.uploadFile(Config.PDF_DOCUMENT_NAME, Config.LOCAL_FOLDER, Config.REMOTE_FOLDER)
@@ -26,17 +26,3 @@ class PdfGetAnnotations:
             else:
                 logging.error(f"get_annotations(): Failed to get annotation in the document. Response code: {response.code}")
                 return annotation_result
-
-    def get_annotation(self, annotation_id):
-        """Get annotation from the page in the PDF document."""
-        if self.pdfApi:
-            self.helper.uploadFile(Config.PDF_DOCUMENT_NAME, Config.LOCAL_FOLDER, Config.REMOTE_FOLDER)
-
-            args = {
-			    "folder": Config.REMOTE_FOLDER
-		    }
-            response = self.pdfApi.get_text_annotation(Config.PDF_DOCUMENT_NAME, annotation_id, **args)
-            if response.code == 200:
-                logging.info(f"get_annotationn(): annotation '{annotation_id}' successfully found '{response.annotation.contents}' in the document '{Config.PDF_DOCUMENT_NAME}'.")
-            else:
-                logging.error(f"get_annotation(): Failed to get annotation in the document. Response code: {response.code}")
