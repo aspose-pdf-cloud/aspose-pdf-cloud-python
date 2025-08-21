dc21fafd1ede5b4a9d32b8be5df327e063bd603d
Kirill Novinskiy <kirill.novinskiy@aspose.com>
2025-08-21 12:22:16 +0000

    update to 25.8.0

--- asposepdfcloud/apis/pdf_api.py
+++ asposepdfcloud/apis/pdf_api.py
@@ -24907,6 +24907,143 @@ class PdfApi(object):
                                         _request_timeout=params.get('_request_timeout'),
                                         collection_formats=collection_formats)
 
+    def post_document_pages_resize(self, name, height, width, pages, **kwargs):
+        """
+        Rsize PDF document.
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.post_document_pages_resize(name, height, width, pages, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param str name: The document name. (required)
+        :param float height: Page Height (required)
+        :param float width: Page width (required)
+        :param str pages: Comma separated list of pages and page ranges. (Example: 1,3-5,8) (required)
+        :param str storage: The document storage.
+        :param str folder: The document folder.
+        :param str password: Base64 encoded password.
+        :return: AsposeResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('callback'):
+            return self.post_document_pages_resize_with_http_info(name, height, width, pages, **kwargs)
+        else:
+            (data) = self.post_document_pages_resize_with_http_info(name, height, width, pages, **kwargs)
+            return data
+
+    def post_document_pages_resize_with_http_info(self, name, height, width, pages, **kwargs):
+        """
+        Rsize PDF document.
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please define a `callback` function
+        to be invoked when receiving the response.
+        >>> def callback_function(response):
+        >>>     pprint(response)
+        >>>
+        >>> thread = api.post_document_pages_resize_with_http_info(name, height, width, pages, callback=callback_function)
+
+        :param callback function: The callback function
+            for asynchronous request. (optional)
+        :param str name: The document name. (required)
+        :param float height: Page Height (required)
+        :param float width: Page width (required)
+        :param str pages: Comma separated list of pages and page ranges. (Example: 1,3-5,8) (required)
+        :param str storage: The document storage.
+        :param str folder: The document folder.
+        :param str password: Base64 encoded password.
+        :return: AsposeResponse
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['name', 'height', 'width', 'pages', 'storage', 'folder', 'password']
+        all_params.append('callback')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method post_document_pages_resize" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'name' is set
+        if ('name' not in params) or (params['name'] is None):
+            raise ValueError("Missing the required parameter `name` when calling `post_document_pages_resize`")
+        # verify the required parameter 'height' is set
+        if ('height' not in params) or (params['height'] is None):
+            raise ValueError("Missing the required parameter `height` when calling `post_document_pages_resize`")
+        # verify the required parameter 'width' is set
+        if ('width' not in params) or (params['width'] is None):
+            raise ValueError("Missing the required parameter `width` when calling `post_document_pages_resize`")
+        # verify the required parameter 'pages' is set
+        if ('pages' not in params) or (params['pages'] is None):
+            raise ValueError("Missing the required parameter `pages` when calling `post_document_pages_resize`")
+
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'name' in params:
+            path_params['name'] = params['name']
+
+        query_params = []
+        if 'height' in params:
+            query_params.append(('height', params['height']))
+        if 'width' in params:
+            query_params.append(('width', params['width']))
+        if 'pages' in params:
+            query_params.append(('pages', params['pages']))
+        if 'storage' in params:
+            query_params.append(('storage', params['storage']))
+        if 'folder' in params:
+            query_params.append(('folder', params['folder']))
+        if 'password' in params:
+            query_params.append(('password', params['password']))
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.\
+            select_header_accept(['application/json'])
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.\
+            select_header_content_type(['application/json'])
+
+        # Authentication setting
+        auth_settings = ['JWT']
+
+        return self.api_client.call_api('/pdf/{name}/resize', 'POST',
+                                        path_params,
+                                        query_params,
+                                        header_params,
+                                        body=body_params,
+                                        post_params=form_params,
+                                        files=local_var_files,
+                                        response_type='AsposeResponse',
+                                        auth_settings=auth_settings,
+                                        callback=params.get('callback'),
+                                        _return_http_data_only=params.get('_return_http_data_only'),
+                                        _preload_content=params.get('_preload_content', True),
+                                        _request_timeout=params.get('_request_timeout'),
+                                        collection_formats=collection_formats)
+
     def post_document_pages_rotate(self, name, rotation_angle, pages, **kwargs):
         """
         Rotate PDF document.
--- docs/PdfApi.md
+++ docs/PdfApi.md
@@ -212,6 +212,7 @@ Method | HTTP request | Description
 [**post_document_image_stamps**](PdfApi.md#post_document_image_stamps) | **POST** /pdf/\{name}/stamps/image | Add document pages image stamps.
 [**post_document_image_stamps_page_specified**](PdfApi.md#post_document_image_stamps_page_specified) | **POST** /pdf/\{name}/stamps/image/pagespecified | Add document image stamps to specified pages.
 [**post_document_page_number_stamps**](PdfApi.md#post_document_page_number_stamps) | **POST** /pdf/\{name}/stamps/pagenumber | Add document page number stamps.
+[**post_document_pages_resize**](PdfApi.md#post_document_pages_resize) | **POST** /pdf/\{name}/resize | Rsize PDF document.
 [**post_document_pages_rotate**](PdfApi.md#post_document_pages_rotate) | **POST** /pdf/\{name}/rotate | Rotate PDF document.
 [**post_document_text_footer**](PdfApi.md#post_document_text_footer) | **POST** /pdf/\{name}/footer/text | Add document text footer.
 [**post_document_text_header**](PdfApi.md#post_document_text_header) | **POST** /pdf/\{name}/header/text | Add document text header.
@@ -5646,6 +5647,34 @@ Name | Type | Description  | Notes
  **stamp** | [**PageNumberStamp**](PageNumberStamp.md)| The stamp. | 
  **start_page_number** | **int**| The start page number. | [optional] 
  **end_page_number** | **int**| The end page number. | [optional] 
+ **storage** | **str**| The document storage. | [optional] 
+ **folder** | **str**| The document folder. | [optional] 
+ **password** | **str**| Base64 encoded password. | [optional] 
+
+### Return type
+
+[**AsposeResponse**](AsposeResponse.md)
+
+### HTTP request headers
+
+ - **Content-Type**: application/json
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **post_document_pages_resize**
+> AsposeResponse post_document_pages_resize(name, height, width, pages, storage=storage, folder=folder, password=password)
+
+Rsize PDF document.
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **name** | **str**| The document name. | 
+ **height** | **float**| Page Height | 
+ **width** | **float**| Page width | 
+ **pages** | **str**| Comma separated list of pages and page ranges. (Example: 1,3-5,8) | 
  **storage** | **str**| The document storage. | [optional] 
  **folder** | **str**| The document folder. | [optional] 
  **password** | **str**| Base64 encoded password. | [optional] 
