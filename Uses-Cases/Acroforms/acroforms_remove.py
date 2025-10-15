4ba47cb22a86785d0de3711c01b1bca1540879b1
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-10-15 10:23:36 +0300

    PDFCLOUD-5185: added snippets for acroforms

---
+++ Uses-Cases/Acroforms/acroforms_remove.py
@@ -0,0 +1,23 @@
+import os
+import logging
+from pathlib import Path
+from acroforms_helper import Config, PdfAcroformsHelper, logging
+from asposepdfcloud import ApiClient, PdfApi, TextBoxField, Rectangle, Border, Dash
+
+class PdfAcroformsDel:
+    def __init__(self, pdf_api: PdfApi, helper: PdfAcroformsHelper):
+        self.pdfApi = pdf_api
+        self.helper = helper
+
+    def delField(self, documentName: str, outputDocumentName: str, fieldName: str, localFolder: Path, remoteFolder: str):
+        self.helper.uploadFile(documentName, localFolder, remoteFolder)
+
+        try:
+            response = self.pdfApi.delete_field(documentName, fieldName, folder=remoteFolder)
+            if response.code == 200:
+                logging.info(f"PdfAcroformsDel(): Form filed '{fieldName}' successfully deleted from docuemnt.")
+                self.helper.downloadFile(documentName, outputDocumentName, localFolder, remoteFolder, "del_form_")
+            else:
+                logging.error(f"PdfAcroformsDel(): Failed to reomve filed '{fieldName}' from document. Response code: {response.code}")
+        except Exception as e:
+            logging.error(f"PdfAcroformsDel(): Error while deleting form field: {e}")
---
+++ Uses-Cases/Acroforms/acroforms_set.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from pathlib import Path
+from acroforms_helper import Config, PdfAcroformsHelper, logging
+from asposepdfcloud import ApiClient, PdfApi, Field, FieldType
+
+class PdfAcroformsSetter:
+    def __init__(self, pdf_api: PdfApi, helper: PdfAcroformsHelper):
+        self.pdfApi = pdf_api
+        self.helper = helper
+
+    def setField(self, documentName: str, outputDocumentName: str, fieldName: str, localFolder: Path, remoteFolder: str):
+        self.helper.uploadFile(documentName, localFolder, remoteFolder)
+
+        field = Field(
+            name="EMail",
+            type=FieldType.TEXT,
+            values=["aspose-pdf-cloud@example.com"]
+        )
+        try:
+            response = self.pdfApi.put_update_field(documentName, fieldName, field, folder=remoteFolder)
+            if response.code == 200:
+                logging.info(f"PdfAcroformsReplace(): Form filed '{fieldName}' successfully updated in the document.")
+                self.helper.downloadFile(documentName, outputDocumentName, localFolder, remoteFolder, "set_form_")
+            else:
+                logging.error(f"PdfAcroformsReplace(): Failed to update filed 'Email' inn the document. Response code: {response.code}")
+        except Exception as e:
+            logging.error(f"PdfAcroformsReplace(): Error while updating form field: {e}")
